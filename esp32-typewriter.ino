009e543cdf0ada5ced1fa9da4fa003ae88a927bd
imcnair10 <118998356+imcnair10@users.noreply.github.com>
2022-11-24 16:24:35 -0700

    working update for esp32 printer typewriter

--- esp32-typewriter.ino
+++ esp32-typewriter.ino
@@ -15,7 +15,6 @@
 
 // track the current horizontal position of the print head. 
 int horizontalPosition = 0;
-int loopCounter = 0;
 
 PS2KeyAdvanced keyboard;
 uint16_t inChar;
@@ -23,7 +22,6 @@ uint16_t inChar;
 void setup() {
   
   
-  keyboard.begin(KEYBOARD_DATA, KEYBOARD_CLK);
   // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
@@ -41,38 +39,40 @@ void setup() {
   Serial2.write(char(247)); // bit positions 5, 6, 7, 8
   Serial2.write(char(0));             // no bit positions
 
-//  // fetch the printer
-//  sleep(1);
-//  Serial.println("Fetching ID");
-//  Serial2.write(char(27));  // ESC
-//  Serial2.write(char(63));  // Send ID command
-//  while (!Serial2.available()) {
-//    sleep(1);
-//  }
-//  while ( Serial2.available() )  {
-//    Serial.print(Serial2.read());
-//  }
-//  Serial.print("\n");
-//  sleep(1);
+  // fetch the printer
+  sleep(1);
+  Serial.println("Fetching ID");
+  Serial2.write(char(27));  // ESC
+  Serial2.write(char(63));  // Send ID command
+  while (!Serial2.available()) {
+    sleep(1);
+  }
+  while ( Serial2.available() )  {
+    Serial.print(Serial2.read());
+  }
+  Serial.print("\n");
+  sleep(1);
   
   Serial.println("Set up unidirectional printing");
   // set the printer to use unidirectional printing
   Serial2.write(byte(27));  // ESC
   Serial2.write(byte(62));  // Turn on unidirectional printing 
 
-  sleep(1);
-  Serial.println("Sent printer select");
-  if (Serial2.availableForWrite() > 0) Serial2.write(byte(17));  // printer select (deselect is 19)
+  //sleep(1);
+  //Serial.println("Sent printer select");
+  //Serial2.write(byte(17));  // printer select (deselect is 19)
 
   sleep(1);
   Serial.println("Serial Txd is on pin: "+String(TX));
   Serial.println("Serial Rxd is on pin: "+String(RX));
   
   Serial.println("starting keyboard handler");
+  keyboard.begin(KEYBOARD_DATA, KEYBOARD_CLK);
+
+
 }
 
 void loop() {
-  int incomingByte = -1;
   char buff[6];
   
   while (keyboard.available()) {
@@ -80,33 +80,38 @@ void loop() {
     inChar = keyboard.read();
     if ( inChar > 0 ) {
       
-        Serial.print(inChar);
+      if (inChar >= 32 && inChar < 127) {
+
+        if (horizontalPosition > 9999) horizontalPosition = 9999;
+        sprintf(buff, "F%04d", horizontalPosition);
+        
+        Serial2.print(char(27));  // ESC
+        Serial2.print(buff);
+        Serial2.print(char(inChar));
+        Serial2.print(char(13));
+        
+        horizontalPosition = horizontalPosition + 8;
+        Serial.print(char(inChar));
+      }
+      else if(inChar == 287) {
+
+        if (horizontalPosition > 9999) horizontalPosition = 9999;
+        sprintf(buff, "F%04d", horizontalPosition);
+        
+        Serial2.print(char(27));  // ESC
+        Serial2.print(buff);
+        Serial2.print(' ');
+        Serial2.print(char(13));
+        
+        horizontalPosition = horizontalPosition + 8;          
+        Serial.print(' ');
+      }
+      else if (inChar == 286) {
+        horizontalPosition = 0;
+        Serial2.print(char(13));
+        Serial2.print(char(10));
+        Serial.print('\n');
+      }
     }
-    /*
-    if (incomingByte == -1) break;
-    else if (incomingByte == 13 || incomingByte == 10) { // intercept returns and newlines.
-      Serial.println("gobbling up carriage return");
-      //Serial2.print(char(13));
-      //Serial2.print(char(10));
-    }
-    else if (incomingByte == 38) {
-      horizontalPosition = 0;
-      Serial2.print(char(13));
-      Serial2.print(char(10));
-    }
-    else {
-      // convert horizontal position to a 4 padded 4 character string decimal.
-      if (horizontalPosition > 9999) horizontalPosition = 9999;
-      sprintf(buff, "F%04d", horizontalPosition);
-      
-      Serial2.print(char(27));  // ESC
-      Serial2.print(buff);
-      Serial2.print(char(incomingByte));
-      Serial2.print(char(13));
-      
-      horizontalPosition = horizontalPosition + 8;
-      
-    }
-    */
   }
 }
